8cec964f017281790cd5a24e9e7b91bc62d89771
Hritik003 <73677045+Hritik003@users.noreply.github.com>
2020-12-08 16:14:49 +0530

    basic input & output

---
+++ input and output.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a,b,c;
+    
+    scanf("%d %d %d",&a,&b,&c);
+    int sum=a+b+c;
+    printf("%d",sum);
+    return 0;
+}
+
