a8a5cb979a4a5920a855765abea2b24ba23dfdd7
Hritik <73677045+Hritik003@users.noreply.github.com>
2022-01-15 00:55:18 +0530

    prefix sum

---
+++ Leetcode 560 Subarray sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int count =0;
+        unordered_map <int ,int> m;
+        int sum =0;
+        for(int i=0;i<nums.size();++i)
+        {
+            sum += nums[i];
+            if(sum ==k)
+            {
+                count++;
+
+            }
+            
+            if(m.find(sum-k) != m.end())
+                count+= m[sum-k];
+            m[sum]++;
+        }
+        return count;
+    }
+};
