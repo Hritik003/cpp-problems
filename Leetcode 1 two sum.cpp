e61ce24520045c8b3beeff9ea825d3c03017319d
Hritik <73677045+Hritik003@users.noreply.github.com>
2022-01-22 18:05:47 +0530

    using maps

---
+++ Leetcode 1 two sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        vector<int> v;
+    unordered_map<int,int> freq;
+    for(int i=0;i<nums.size();i++){
+            int s = target - nums[i];
+            if(freq.find(s)!=freq.end()){
+                v.push_back(i);
+                v.push_back(freq[s]);      
+            }
+        freq[nums[i]] = i;
+        }
+        return v;
+        
+    }
+        
+        
+        
+        
+    
+};
