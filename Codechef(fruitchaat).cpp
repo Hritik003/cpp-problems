1ed2d9edf4e55cc83dfb307cacc5fe76379a0e57
Hritik <73677045+Hritik003@users.noreply.github.com>
2022-02-09 23:29:50 +0530

    Add files via upload

---
+++ Codechef(fruitchaat).cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    
+	{
+	int y,x;
+	cin>>x>>y;
+	x/2<=y ? cout<< x/2 <<"\n" : cout<< y<<"\n";
+	}
+	
+
+	return 0;
+}
+
