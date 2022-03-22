67027749048778ef03a0009842f261b33ed888ba
Hritik <73677045+Hritik003@users.noreply.github.com>
2022-03-22 12:21:45 +0530

    Count hills and valleys

---
+++ Leetcode 2210.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int countHillValley(vector<int>& nums) {
+        int n = nums.size();
+       
+        vector<int> v;
+         v.push_back(nums[0]);
+         
+       for(int i=1;i<n;++i)
+       {
+           if(nums[i] != nums[i-1])
+           {
+               v.push_back(nums[i]);
+           }
+       }
+        int c=0;
+        for(int i=1;i<v.size()-1;++i)
+        {
+           
+            if((v[i]>v[i+1] && v[i]>v[i-1] )|| (v[i]<v[i+1] && v[i]<v[i-1]))
+            {
+             
+                
+                c++;
+                
+            }
+           
+        }
+
+        return c;
+    }
+};
