a8dc02d85f7b05114ae7e13cbbf3b9957d9d35bc
Hritik <73677045+Hritik003@users.noreply.github.com>
2022-03-24 22:48:35 +0530

    Game

---
+++ 1649A.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,one=0,zero=0;
+        int count=0;
+        cin>>n;
+        vector<int> v(n);
+        for(int x : v)
+        {
+            cin>>x;
+            if(x==0)zero++;        }
+        if(zero==0)
+        {
+            cout<<0<<"\n";
+        }
+        else
+        {
+            cout<<2*zero<<"\n";
+        }
+    }
+
+    return 0;
+}
