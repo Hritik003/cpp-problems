7f1033fc50495925556dab333ea888ec5a49d124
Hritik <hritikraj_03@outlook.com>
2022-07-20 18:06:12 +0530

    missing number

---
+++ Leetcode268.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int sum = nums.size();
+        for(int i =0;i<nums.size();++i){
+            sum+= i-nums[i];
+        }
+        return sum;
+    }       
+};
