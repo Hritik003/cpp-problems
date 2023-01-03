12f4aee51e65b09f8961e64ef72f09b30566c0e1
Hritik <73677045+Hritik003@users.noreply.github.com>
2023-01-03 17:16:04 +0530

    backtracking

---
+++ Recursion/Print linearly from 1 to N.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+void f(int i,int n){
+    if(i<1)
+    return;
+    
+    f(i-1,n);
+    cout<<i<<"\n";
+}
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<"The recursive stack:"<<"\n";
+    f(n,n);
+
+    return 0;
+}
