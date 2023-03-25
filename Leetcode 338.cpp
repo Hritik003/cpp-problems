be5df5c94c0685d876aad6207068aa2af29ec907
Hritik <73677045+Hritik003@users.noreply.github.com>
2023-03-25 21:46:16 +0530

    Counting Bits

--- Leetcode 338.cpp
+++ Leetcode 338.cpp
@@ -27,3 +27,20 @@ public:
         return count;
     }
 };
+
+
+
+//optimized solution
+
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int>ans;
+        for(int i=0;i<=n;i++){
+            int x = __builtin_popcount(i);
+            ans.push_back(x);
+
+        }
+        return ans;
+    }
+};
